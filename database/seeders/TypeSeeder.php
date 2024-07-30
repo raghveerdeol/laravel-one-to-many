803303f3761f2d6a796601d280fdee6bec7183d6
raghveerdeol <deolraghveer5@gmail.com>
2024-07-30 13:48:15 +0200

    aggiunti dati attraverso seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $typesName = [
+            'static',
+            'front-end',
+            'back-end',
+            'full-stack',
+        ];
+
+        foreach($typesName as $typeName){
+            $type = new Type();
+            $type->name = $typeName;
+            $type->color = $faker->unique()->safeHexColor();
+            $type->save();
+        }
     }
 }
